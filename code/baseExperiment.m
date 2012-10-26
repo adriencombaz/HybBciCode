95338bb0a7f5f1dd5c1417d08d5bef8d3fa5dda8
Adrien Combaz <adriencombaz@gmail.com>
2012-10-26 15:55:24 +0200

    for testing first push

--- code/baseExperiment.m
+++ code/baseExperiment.m
@@ -1,5 +1,7 @@
 function baseExperiment
 
+% test modification
+
 %%                        SCANNER PARAMETERS
 %==========================================================================
 subjectName                     = 'testMarkers';
