8f493da0104aa86c1bbe7411ea076f937acb076b
Adrien Combaz <adriencombaz@gmail.com>
2013-03-14 17:45:25 +0100

    working for linux machines

--- dataAnalysisCodes/watchERP/02-ter-p3Classification/buildSvmClassifier_perSubject.m
+++ dataAnalysisCodes/watchERP/02-ter-p3Classification/buildSvmClassifier_perSubject.m
@@ -1,13 +1,10 @@
 function buildSvmClassifier_perSubject( iS )
 
-addpath( genpath( 'deps/' ) );
-rmpath( genpath('deps/eeglab10_0_1_0b/external/SIFT_01_alpha') );
-
-    
 % init host name
 %--------------------------------------------------------------------------
 if isunix,
     envVarName = 'HOSTNAME';
+    rootDir = '~/PhD/hybridBCI-stuffs/';
 else
     envVarName = 'COMPUTERNAME';
 end
@@ -18,27 +15,31 @@ switch hostName,
         addpath( genpath('d:\KULeuven\PhD\Work\Hybrid-BCI\HybBciCode\dataAnalysisCodes\deps\') );
         dataDir = 'd:\KULeuven\PhD\Work\Hybrid-BCI\HybBciRecordedData\watchERP\';
         resDir = 'd:\KULeuven\PhD\Work\Hybrid-BCI\HybBciProcessedData\watch-ERP\';
+        codeDir = 'd:\KULeuven\PhD\Work\Hybrid-BCI\HybBciCode\dataAnalysisCodes\watchERP\';
     case 'neu-wrk-0158',
         addpath( genpath('d:\Adrien\Work\Hybrid-BCI\HybBciCode\dataAnalysisCodes\deps\') );
         addpath( genpath('d:\Adrien\matlabToolboxes\eeglab10_0_1_0b\') );
         rmpath( genpath('d:\Adrien\matlabToolboxes\eeglab10_0_1_0b\external\SIFT_01_alpha') );
         dataDir = 'd:\Adrien\Work\Hybrid-BCI\HybBciRecordedData\watchERP\';
         resDir = 'd:\Adrien\Work\Hybrid-BCI\HybBciProcessedData\watch-ERP\';
-    case 'sunny'
-        addpath( genpath( 'deps/' ) );
-        rmpath( genpath('deps/eeglab10_0_1_0b/external/SIFT_01_alpha') );
-        dataDir = 'data/';
-        resDir = 'results/';
+        codeDir = 'd:\Adrien\Work\Hybrid-BCI\HybBciCode\dataAnalysisCodes\watchERP\';
+    case {'sunny', 'solaris'}
+        addpath( genpath( '~/PhD/hybridBCI-stuffs/deps/' ) );
+        rmpath( genpath('~/PhD/hybridBCI-stuffs/deps/eeglab10_0_1_0b/external/SIFT_01_alpha') );
+        dataDir = '~/PhD/hybridBCI-stuffs/data/';
+        resDir = '~/PhD/hybridBCI-stuffs/results/';
+        codeDir = '~/PhD/hybridBCI-stuffs/code/';
     otherwise,
         error('host not recognized');
 end
 
-% dataDir = 'data/';
-
-% TableName   = '..\01-preprocess-plot\watchErpDataset.xlsx';
-TableName   = '..\01-preprocess-plot\watchErpDataset2.xlsx';
-fileList    = dataset('XLSFile', TableName);
-
+if isunix,
+    TableName   = fullfile( codeDir, '01-preprocess-plot', filesep, 'watchErpDataset2.csv');
+    fileList    = dataset('File', TableName, 'Delimiter', ',');
+else
+    TableName   = fullfile( codeDir, '01-preprocess-plot', filesep, 'watchErpDataset2.xlsx');
+    fileList    = dataset('XLSFile', TableName);
+end
 
 sub     = unique( fileList.subjectTag );
 cond    = unique( fileList.condition );
